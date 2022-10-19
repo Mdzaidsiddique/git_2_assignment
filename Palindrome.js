e88dedf888149273e984dbf1ed1cdda0ba5919cb
Madhumitha reddy <you@example.com>
2022-10-19 20:23:25 +0530

    palindrome edited

--- Palindrome.js
+++ Palindrome.js
@@ -1,6 +1,6 @@
 let str = 'madam';
 let str_p = '';
-for(let i = str.length-1; i>=0; i++){ //should be i--
+for(let i=str.length-1; i>=0; i--){ //should be i--
   str_p +=str[i];
 }
 if(str == str_p){
